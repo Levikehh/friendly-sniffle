156c11f3a210e18137e51e13617d9dccc2a8bfa4
Dóczi Levente <bkalevi@gmail.com>
2023-04-21 16:24:09 +0200

    Few changes

--- constants.ts
+++ constants.ts
@@ -1,6 +1,7 @@
 const ERROR_MESSAGES = {
     "400": "You need to log in!",
-    "401": "You don't have permission to perform such action!"
+    "401": "You don't have permission to perform such action!",
+    "500": "Internal server error. Please try again later",
 }
 
 const SCHEMA_ERROR_MESSAGES = {
--- middleware/access.ts
+++ middleware/access.ts
@@ -1,14 +1,17 @@
 import { NextFunction, Request, Response } from "express";
 
-const {ERROR_MESSAGES} = require("@/constants")
+const { ERROR_MESSAGES } = require("@/constants");
 
 const hasAccess = (minimumAccessLevel: number) => {
   return (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.cookies)
     if (!req.body.userId) return res.status(400).send(ERROR_MESSAGES["400"]);
-    if (!req.body.accessLevel) return res.status(400).send(ERROR_MESSAGES["400"]);
-    if (Number(req.body.accessLevel) < Number(minimumAccessLevel)) return res.status(401).send(ERROR_MESSAGES["401"]);
-    delete req.body.userId
-    delete req.body.accessLevel
+    if (!req.body.accessLevel)
+      return res.status(400).send(ERROR_MESSAGES["400"]);
+    if (Number(req.body.accessLevel) < Number(minimumAccessLevel))
+      return res.status(401).send(ERROR_MESSAGES["401"]);
+    delete req.body.userId;
+    delete req.body.accessLevel;
     next();
   };
 };
